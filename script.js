8604b06e6344e472410bb8b499f80ac26bdfed06
Nelson Sabono <nelson.sab16@gmail.com>
2022-07-14 23:23:51 +0200

    Ajout du script.js avec fonction le tableau change quand on click sur les boutons

---
+++ script.js
@@ -0,0 +1,21 @@
+let table1 = document.querySelector(".table1");
+let table2 = document.querySelector(".table2");
+let table3 = document.querySelector(".table3");
+
+function table_1() {
+    table1.style.display = '';
+    table2.style.display = 'none';
+    table3.style.display = 'none';
+}
+
+function table_2() {
+    table2.style.display = '';
+    table1.style.display = 'none';
+    table3.style.display = 'none';
+}
+
+function table_3() {
+    table3.style.display = '';
+    table1.style.display = 'none';
+    table2.style.display = 'none';
+}
