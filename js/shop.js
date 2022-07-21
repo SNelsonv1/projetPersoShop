9bbb96c0b53540ea0b2ca339c693ead4efffc054
Nelson Sabono <nelson.sab16@gmail.com>
2022-07-21 22:24:13 +0200

    Suppression des onclick sur le HTML et modification des scripts, Modification du script pour selectionner les cellules, fonctionne

--- js/shop.js
+++ js/shop.js
@@ -1,29 +1,36 @@
-let table1 = document.querySelector(".table1");
-let table2 = document.querySelector(".table2");
-let table3 = document.querySelector(".table3");
+let table1 = document.querySelector('.table1');
+let table2 = document.querySelector('.table2');
+let table3 = document.querySelector('.table3');
 
+let nourriture = document.querySelector('.nourriture')
 function table_1() {
     table1.style.display = '';
     table2.style.display = 'none';
     table3.style.display = 'none';
 }
+nourriture.addEventListener('click', table_1);
 
+let materiel = document.querySelector('.materiel')
 function table_2() {
     table2.style.display = '';
     table1.style.display = 'none';
     table3.style.display = 'none';
 }
+materiel.addEventListener('click', table_2);
 
+let autre = document.querySelector('.autre')
 function table_3() {
     table3.style.display = '';
     table1.style.display = 'none';
     table2.style.display = 'none';
 }
+autre.addEventListener('click', table_3);
 
-let testElement = document.querySelector('td');
 
+let testElement = document.getElementsByTagName('td');
 function signal() {
     console.log('Ok')
 }
-
-testElement.addEventListener('click', signal);
+for (let i = 0; i < testElement.length; i++) {
+    testElement[i].addEventListener('click', signal);
+}
